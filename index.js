a82cdadebdbce2cd426ae3f03369942edf75e8de
Sukriti Sharma <153065339+sukkuzzz@users.noreply.github.com>
2024-06-14 09:26:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "Ramram@1",
+  port: 5432,
+});
+db.connect();
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [];
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT *FROM items ORDER BY id ASC ");
+    items = result.rows;
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (error) {
+    console.error("something is wrong with the server", error.message);
+  }
+});
+
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+  try {
+    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    res.redirect("/");
+  } catch (error) {
+    console.error("something is wrong with the server ", error.message);
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  const item = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+  try {
+    await db.query("UPDATE items SET title=($1) WHERE id=($2)", [item, id]);
+    res.redirect("/");
+  } catch (error) {
+    console.error("something is wrong with the server ", error.message);
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    await db.query("DELETE FROM items WHERE id=($1)", [id]);
+    res.redirect("/");
+  } catch (error) {
+    console.error("something is wrong with the server", error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
